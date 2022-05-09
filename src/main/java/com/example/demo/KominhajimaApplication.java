c97dfe3ef80c8f74a8aff94d67d822e4485b408b
BenYoo-ben <neverknown0909@gmail.com>
2022-05-09 22:24:18 +0900

    Initial Setup

---
+++ src/main/java/com/example/demo/KominhajimaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KominhajimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KominhajimaApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
