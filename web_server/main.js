3a39dbc765381d48739d755073a72fcfc3a9d5d9
hun97 <kjeonghun920@gmail.com>
2022-05-31 18:09:57 +0900

    Rename: main.js

--- web_server/main.js
+++
@@ -1,7 +0,0 @@
-const TCPClient = require('./tcp-client');
-
-var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
-
-client.get_dir_info('/home').then((file) => {
-    console.log(file);
-});
