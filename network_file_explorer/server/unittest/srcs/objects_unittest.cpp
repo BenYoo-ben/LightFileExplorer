8639f3e2c43b26f073ecfb1a0291b370008dd282
BenYoo-ben <neverknown0909@gmail.com>
2022-06-02 10:00:50 +0100

    Modified: modified lock to use ref and added unittest
    '=' operator on std::map creates new copy.
    lock algorithm was using '=' operator on maps to add/remove locks, so locks were not being affected globally.
    Now add, remove functions use references to them so bug is fixed.
    
    Added unittest to ensure lock is working.

--- network_file_explorer/server/unittest/srcs/objects_unittest.cpp
+++ network_file_explorer/server/unittest/srcs/objects_unittest.cpp
@@ -2,10 +2,45 @@
 
 #include "objects.hpp"
 
-TEST (SAMPLE, SAMPLE) {
-    
-    ASSERT_EQ(1,2);
+TEST (OBJECT, TEST_LOCK) {
+    lock_handler lh;
 
+    std::string testString("TESTSTRING");
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), true);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), true);
+
+    lh.add_lock(lh.SOFT_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), false);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), true);
+
+    lh.remove_lock(lh.SOFT_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), true);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), true);
+
+    lh.add_lock(lh.HARD_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), true);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), false);
+
+    lh.remove_lock(lh.HARD_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), true);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), true);
+
+    lh.add_lock(lh.SOFT_LOCK, testString);
+    lh.add_lock(lh.HARD_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), false);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), false);
+
+    lh.remove_lock(lh.SOFT_LOCK, testString);
+    lh.remove_lock(lh.HARD_LOCK, testString);
+
+    ASSERT_EQ(lh.check_lock(lh.SOFT_LOCK, testString), true);
+    ASSERT_EQ(lh.check_lock(lh.HARD_LOCK, testString), true);
 }
 
 
