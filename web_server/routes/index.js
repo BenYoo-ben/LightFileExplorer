ae799ca168f186b222d0a3cff375179075e0efec
hun97 <kjeonghun920@gmail.com>
2022-06-02 19:56:12 +0900

    Create: JSON sort

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -6,6 +6,12 @@ var TCPClient = require('../tcp-client');
 router.get('/', function (req, res, next) {
     var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
     client.get_dir_info('/home').then((json) => {
+        json.sort((a, b) => {
+            if (a.is_dir == b.is_dir) {
+                return ('' + a.name).localeCompare(b.name);
+            }
+            return a.is_dir < b.is_dir ? 1 : -1;
+        });
         res.render('index', { json: json, cur_dir: '/home' });
         client.socket.destroy();
     });
@@ -14,9 +20,14 @@ router.get('/', function (req, res, next) {
 router.get('/:dir', function (req, res, next) {
     var dir = req.params.dir;
     const decoded = decodeURIComponent(dir);
-    console.log(decoded);
     var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
     client.get_dir_info('/home' + decoded).then((json) => {
+        json.sort((a, b) => {
+            if (a.is_dir == b.is_dir) {
+                return ('' + a.name).localeCompare(b.name);
+            }
+            return a.is_dir < b.is_dir ? 1 : -1;
+        });
         res.render('index', { json: json, cur_dir: '/home' + decoded });
         client.socket.destroy();
     });
