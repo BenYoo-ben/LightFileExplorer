18876b593a5d54fef9ec3a284db57b2d5b2905d0
hun97 <kjeonghun920@gmail.com>
2022-06-03 13:25:59 +0900

    Modified: hide public ip

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var TCPClient = require('../tcp-client');
+
+var tcp_server = fs.readFileSync('tcp-server.json');
+tcp_server = JSON.parse(tcp_server);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
+    var client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
     client.get_dir_info('/home').then((json) => {
         json.sort((a, b) => {
             if (a.is_dir == b.is_dir) {
@@ -20,7 +24,7 @@ router.get('/', function (req, res, next) {
 router.get('/:dir', function (req, res, next) {
     var dir = req.params.dir;
     const decoded = decodeURIComponent(dir);
-    var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
+    var client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
     client.get_dir_info('/home' + decoded).then((json) => {
         json.sort((a, b) => {
             if (a.is_dir == b.is_dir) {
--- web_server/test-tcp-client.js
+++ web_server/test-tcp-client.js
@@ -1,7 +1,10 @@
 const TCPClient = require('./tcp-client');
+const fs = require('fs');
+let tcp_server = fs.readFileSync('./tcp-server.json', { encoding: 'utf-8' });
 
-var client = new TCPClient({ port: 55551, host: '218.48.36.143' });
+tcp_server = JSON.parse(tcp_server);
 
+var client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
 client.get_dir_info('/home').then((file) => {
     console.log(file);
 });
