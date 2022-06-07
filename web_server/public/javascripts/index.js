8fae22ae46c240813055b42d8798d63f10ef8f48
hun97 <kjeonghun920@gmail.com>
2022-06-07 20:32:44 +0900

    Feat: added dropdown button
    Replaced the download button with dropdown button that contains download button and others.
    Other buttons are added for latter purpose.
    Used bootstrap breadcrumb for navigating directories.

--- web_server/public/javascripts/index.js
+++ web_server/public/javascripts/index.js
@@ -3,25 +3,32 @@ const currentUrl = window.location.href;
 $(function () {
     const dirs = cur_dir.split('/');
     let url = '/';
+    let ol = $('<ol>');
+    ol.addClass('breadcrumb');
     for (let i = 0; i < dirs.length; i++) {
-        let a = $('<a>');
-        a.text(dirs[i]);
-        if (i === 0) {
-            a.attr('href', url);
+        let li = $('<li>');
+        li.addClass('breadcrumb-item');
+        if (i === dirs.length - 1) {
+            li.text(dirs[i]);
+            li.addClass('active');
+        } else if (i === 0) {
+            let a = $('<a>').attr('href', url).text(dirs[i]);
+            li.append(a);
         } else {
-            $('#dir').append($('<span>').text(' > '));
             let encoded = encodeURIComponent('/' + dirs[i]);
             url = url + encoded;
-            a.attr('href', url);
+            let a = $('<a>').attr('href', url).text(dirs[i]);
+            li.append(a);
         }
-        $(a).appendTo('#dir');
+        ol.append(li);
     }
+    $('#dir').append(ol);
     let thead = $('<thead>').appendTo('.table');
     let tr = $('<tr>').appendTo(thead);
     let ico = $('<img>');
-    ico.attr('src', '/images/folder.svg');
+    ico.attr('src', '/images/file-earmark.svg');
     ico.attr('alt', 'folder');
-    tr.addClass('table-secondary fs-5 fw-bold');
+    tr.addClass('table-secondary fw-bold align-middle');
     tr.append($('<td>').addClass('icon').append(ico));
     tr.append($('<td>').addClass('name').text('Name'));
     tr.append($('<td>').addClass('time').text('Modified'));
@@ -30,50 +37,51 @@ $(function () {
     for (let i = 0; i < json.length; i++) {
         let tr = $('<tr>').appendTo('.table');
         if (json[i]['is_dir'] === '1') {
-            let td = $('<td>');
+            let td_icon = $('<td>');
             let ico = $('<img>');
-            td.addClass('icon');
+            let td_name = $('<td>');
+            const encoded = encodeURIComponent('/' + json[i]['name']);
+            td_icon.addClass('icon');
             ico.attr('src', '/images/folder.svg');
             ico.attr('alt', 'folder');
-            td.append(ico);
-            tr.append(td);
+            td_icon.append(ico);
+            tr.append(td_icon);
+            td_name.addClass('name');
+            td_name.append($(`<a href="${currentUrl}${encoded}"></a>`).text(json[i]['name']));
+            tr.append(td_name);
+            tr.append($('<td>').text(json[i]['time']).addClass('time'));
+            tr.append($('<td>').text('-').addClass('size'));
         } else {
-            let td = $('<td>');
+            let td_icon = $('<td>');
             let ico = $('<img>');
-            td.addClass('icon');
+            td_icon.addClass('icon');
             ico.attr('src', '/images/file-earmark.svg');
             ico.attr('alt', 'file');
-            td.append(ico);
-            tr.append(td);
+            td_icon.append(ico);
+            tr.append(td_icon);
+            tr.append($('<td>').text(json[i]['name']).addClass('name'));
+            tr.append($('<td>').text(json[i]['time']).addClass('time'));
+            tr.append($('<td>').text(json[i]['size']).addClass('size'));
         }
 
-        if (json[i]['is_dir'] === '1') {
-            let td = $('<td>');
-            td.addClass('name');
-            const encoded = encodeURIComponent('/' + json[i]['name']);
-            td.append($(`<a href="${currentUrl}${encoded}"></a>`).text(json[i]['name']));
-            tr.append(td);
-        } else {
-            tr.append($('<td>').text(json[i]['name']).addClass('name'));
-        }
-        tr.append($('<td>').text(json[i]['time']).addClass('time'));
+        let td_dropdown = $('<td>');
+        td_dropdown.addClass('dropdown');
+        td_dropdown.html(`
+        <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/images/three-dots-vertical.svg" alt="dropdown"/>
+        </a>`);
+        let dropdown_menu = $(`<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1"></ul>`);
         if (json[i]['is_dir'] === '0') {
-            let td = $('<td>');
-            let a = $('<a>');
-            let ico = $('<img>');
             const encoded = encodeURIComponent('/' + json[i]['name']);
             let url = currentUrl + encoded + '/download';
-            tr.append($('<td>').text(json[i]['size']).addClass('size'));
-            td.addClass('download');
-            a.attr('href', url);
-            ico.attr('src', '/images/arrow-down-square.svg');
-            ico.attr('alt', 'file');
-            a.append(ico);
-            td.append(a);
-            tr.append(td);
-        } else {
-            tr.append($('<td>').text('-').addClass('size'));
-            tr.append($('<td>').addClass('download'));
+            dropdown_menu.append($('<li>').append($('<a>').text('Download').addClass('dropdown-item').attr('href', url)));
         }
+        dropdown_menu.append($('<li>').append($('<a>').text('Delete').addClass('dropdown-item').attr('href', '#')));
+        dropdown_menu.append($('<li>').append($('<a>').text('Move to').addClass('dropdown-item').attr('href', '#')));
+        dropdown_menu.append($('<li>').append($('<a>').text('Copy to').addClass('dropdown-item').attr('href', '#')));
+        dropdown_menu.append($('<li>').append($('<a>').text('Rename').addClass('dropdown-item').attr('href', '#')));
+
+        td_dropdown.append(dropdown_menu);
+        tr.append(td_dropdown);
     }
 });
