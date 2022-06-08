93fba64eb8036f0bbc9f62d3bf20ce3c747cc703
hun97 <kjeonghun920@gmail.com>
2022-06-08 18:42:42 +0900

    Fix: root page redirects to /home
    Modified page urls to start with home directory.
    Root page will redirect to /home.
    
    modified:   routes/index.js

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -9,16 +9,7 @@ tcp_server = JSON.parse(tcp_server);
 
 // Get Home directory's info
 router.get('/', async (req, res, next) => {
-    let client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
-    let json = await client.get_dir_info('/home');
-    json.sort((a, b) => {
-        if (a.is_dir == b.is_dir) {
-            return ('' + a.name).localeCompare(b.name);
-        }
-        return a.is_dir < b.is_dir ? 1 : -1;
-    });
-    res.render('index', { json: json, cur_dir: 'home' });
-    client.socket.destroy();
+    res.redirect('/home');
 });
 
 // Get :dir's info
@@ -26,14 +17,14 @@ router.get('/:dir', async (req, res, next) => {
     let dir = req.params.dir;
     const decoded = decodeURIComponent(dir);
     let client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
-    let json = await client.get_dir_info('/home' + decoded);
+    let json = await client.get_dir_info('/' + decoded);
     json.sort((a, b) => {
         if (a.is_dir == b.is_dir) {
             return ('' + a.name).localeCompare(b.name);
         }
         return a.is_dir < b.is_dir ? 1 : -1;
     });
-    res.render('index', { json: json, cur_dir: 'home' + decoded });
+    res.render('index', { json: json, cur_dir: decoded });
     client.socket.destroy();
 });
 
@@ -42,12 +33,9 @@ router.get('/:dir/download', async (req, res, next) => {
     let dir = req.params.dir;
     let decoded = decodeURIComponent(dir);
     let req_dir = path.dirname(decoded) + '/';
-    if (req_dir === '//') {
-        req_dir = '/';
-    }
     decoded = decoded.replace(req_dir, '');
     let client = new TCPClient({ port: tcp_server.port, host: tcp_server.host });
-    let file = await client.download_file('/home' + req_dir, decoded);
+    let file = await client.download_file('/' + req_dir, decoded);
     res.writeHead(200, {
         'Content-disposition': 'attachment;filename=' + decoded,
         'Content-Length': file.length,
