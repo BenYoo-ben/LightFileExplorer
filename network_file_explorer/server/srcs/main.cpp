2931ab22c769eaafb28d058473ef7af2a78a09c8
BenYoo-ben <neverknown0909@gmail.com>
2022-06-15 01:59:49 +0100

    Modified: File Server Port as CL Argument

--- network_file_explorer/server/srcs/main.cpp
+++ network_file_explorer/server/srcs/main.cpp
@@ -3,18 +3,33 @@
 #include "nets.hpp"
 #include "file_scouter.hpp"
 #include "objects.hpp"
+#include "global.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
     server_object so;
+
+    if (argc > 2) {
+        fprintf(stderr, 
+                "This program can only have 1 argument or no argument.\n "
+                "$ %s [optional:Port Number] \n", argv[0]);
+    }
 
     if (so.server_socket_init() < 0) {
         std::cout << "Server Socket Init Failed..." << std::endl;
         exit(1);
     }
 
-    if (so.server_socket_bind() < 0) {
-        std::cout << "Server Socket Bind Failed..." << std::endl;
-        exit(2);
+    if (argc == 1) {
+        if (so.server_socket_bind(global_server_port) < 0) {
+            std::cout << "Server Socket Bind Failed..." << std::endl;
+            exit(2);
+        }
+    } else if (argc == 2) {
+        uint16_t sPort = (uint16_t)(atoi(argv[1]));
+        if (so.server_socket_bind(sPort) < 0) {
+            std::cout << "Server Socket Bind Failed..." << std::endl;
+            exit(2);
+        }   
     }
 
     if (so.server_socket_listen() < 0) {
--- network_file_explorer/server/srcs/nets.cpp
+++ network_file_explorer/server/srcs/nets.cpp
@@ -13,13 +13,13 @@ int server_object::server_socket_init() {
     return 0;
 }
 
-int server_object::server_socket_bind() {
+int server_object::server_socket_bind(uint16_t sPort) {
     struct sockaddr_in server_address;
 
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(global_server_port);
+    server_address.sin_port = htons(sPort);
 
     printf("s_sock : %d\n", s_sock);
     int ret = bind(s_sock, (struct sockaddr *) &server_address,
