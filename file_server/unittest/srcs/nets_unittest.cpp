f4f11e68061aef8c0e090cc700a0def6166903e3
Byongjin Yoo <neverknown0909@gmail.com>
2022-12-15 17:50:09 +0900

    remove: nets start_server test
    The test results diffrently depending from environments.

--- file_server/unittest/srcs/nets_unittest.cpp
+++ file_server/unittest/srcs/nets_unittest.cpp
@@ -80,48 +80,6 @@ TEST (NETS, SERVER_SOCKET_LISTEN_TEST) {
     EXPECT_EQ(badFdObj2.server_socket_listen() < 0, true);
 }
 
-    
-TEST (NETS, SERVER_SOCKET_START) {
-    uint16_t randPort = rand() % 25000 + 20000;
-    
-    // prepare socket for test;
-    int c_sock1 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    struct sockaddr_in dest_address;
-    memset(&dest_address, 0x00 , sizeof(struct sockaddr_in));
-
-    int c_sock2 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    dest_address.sin_family = AF_INET;
-    dest_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    dest_address.sin_port = htons(randPort);
-
-    int retServer = 0;
-    server_object* server = new server_object();
-    std::thread t1([randPort, &server, &retServer](){
-        EXPECT_EQ(server->server_socket_init(), 0);
-        EXPECT_EQ(server->server_socket_bind(randPort), 0);
-        EXPECT_EQ(server->server_socket_listen(), 0);
-
-        retServer = server->server_socket_start();
-    });
-
-    // wait til server is initialized;
-    sleep(2);
-
-    // should be successful
-    ASSERT_EQ(retServer, 0);
-    EXPECT_EQ(connect(c_sock1, (struct sockaddr *)&dest_address, sizeof(dest_address)) >= 0 , true);
-    ASSERT_EQ(retServer, 0);
-   
-    // should fail since listening socket is closed
-    EXPECT_EQ(server->server_socket_close(), 0);
-    EXPECT_EQ(connect(c_sock2, (struct sockaddr *)&dest_address, sizeof(dest_address)) < 0 , true);
-    t1.join();
-
-    ASSERT_EQ(retServer < 0, true);
-}
-
 TEST (NETS, SERVER_SOCKET_CLOSE) {
     server_object server;
     uint16_t randPort = rand() % 25000 + 20000;
